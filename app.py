2ba7db1314af698490313494c87e3f34f51313b9
jeexuan3415 <zhixuann09@gmail.com>
2026-01-21 08:09:02 +0000

    1

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods=["POST"])
+def main():
+    return(render_template("main.html"))
+
 if __name__ == "__main__":
     app.run()
     
